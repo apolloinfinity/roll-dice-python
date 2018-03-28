d72cdb0eeb3b056a3feb54e2a20725f88fe78912
Javier Ramirez <apollo.infinity88@gmail.com>
2018-03-27 23:16:25 -0700

    Update
    Updated some of the code as the randomizer wasn't quite working well. I moved the dice variables inside the while loop and that seems to have remedied the problem. I also updated using formatted string literals.

--- roll_dice.py
+++ roll_dice.py
@@ -1,18 +1,20 @@
-"""Small program that simulates the rolling of a dice.
-
+"""roll-dice.py, By Javier Ramirez, 03-16-2018
+Updated: 3-27-18
+Small program that simulates the rolling of a dice. Still a work in progress but got the MVP working.
 """
 import random as r
 
 def roll_dice():
     sides = 6
-    dice_1 = r.randint(1, sides)
-    dice_2 = r.randint(1, sides)
     rolling = True
     while rolling:
+        dice_1 = r.randint(1, sides)
+        dice_2 = r.randint(1, sides)
         roll_again = input("Ready to roll?\nY for yes or N for no. ")
         if roll_again.lower() != "n":
             print(f'First dice rolled a {dice_1}')
             print(f'Second dice rolled a {dice_2}')
+            print(f'Your total is {dice_1 + dice_2}')
         else:
             rolling = False
     
